cd63a32b32e78cd069242668b95d7b81f83eccf2
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2019-04-05 14:17:37 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -9,10 +9,12 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
-self.artists.length
+    self.artists.length
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    self.artists.collect do |a|
+      a.name
   end
 end
